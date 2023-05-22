ccaf9e9460039d80826914f71e0dd352fa4b390a
analyceferreira <anallyce_@live.com>
2023-05-21 20:55:52 -0400

    FEAT: add type validation on the link route

--- src/routes/link.routes.ts
+++ src/routes/link.routes.ts
@@ -1,8 +1,10 @@
 import express from 'express';
 import { create as createLink, deleteLink, getLinks, redirectToUrl } from '../controllers/link.controllers';
+import { validate } from '../middlewares/zod.validate';
+import { bodyReqLinkSchema, linkPathParams } from '../schemas/link.schemas';
 
 export const linkRoutes = express.Router();
 
 linkRoutes.get('/', getLinks  );
-linkRoutes.post('/', createLink  );
-linkRoutes.delete('/:short_name', deleteLink );
+linkRoutes.post('/', validate(bodyReqLinkSchema), createLink  );
+linkRoutes.delete('/:short_name', validate(linkPathParams), deleteLink );
--- src/schemas/link.schemas.ts
+++ src/schemas/link.schemas.ts
@@ -5,12 +5,16 @@ export const linkSchema = z.object({
     original_url: z.string().url('Informe uma URL válida').min(1, 'URL é obrigatória'),
     short_name: z.string().min(3, 'Short name must contain at least 3 characters').max(100, 'Shortned '),
     
-    userId: z.string() || null,
+    userId: z.string().optional(),
 });
 
 
+export const bodyReqLinkSchema = z.object({
+    body: linkSchema
+});
+
 export const linkPathParams = z.object({
-    short_name: z.string()
+    body: z.object({short_name: z.string()})
 })
 
 
